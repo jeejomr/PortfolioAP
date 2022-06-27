e046dc75928bb50139e287d62470b1fa176ad9bd
jeejomr <95946190+jeejomr@users.noreply.github.com>
2022-06-27 15:37:44 -0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ImgComponent } from './components/img/img.component';
+import { RedSocialComponent } from './components/red-social/red-social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ImgComponent
+    ImgComponent,
+    RedSocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
